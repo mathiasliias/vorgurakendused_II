1fd1e291fa81e9d3a0a51004df61034b0cca2b7d
mathias.liias <mathias.liias@gmail.com>
2018-02-16 10:29:11 +0200

    Http server Http request handler

---
+++ p2p_project/src/httpserver/HttpServer.java
@@ -0,0 +1,45 @@
+package httpserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+    public static void main(String args[]) {
+        int port;
+        ServerSocket server_socket;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            port = 1500;
+        }
+        try {
+
+            server_socket = new ServerSocket(port);
+            System.out.println("httpServer running on port "
+                    + server_socket.getLocalPort());
+
+            // server infinite loop
+            while (true) {
+                Socket socket = server_socket.accept();
+                System.out.println("New connection accepted "
+                        + socket.getInetAddress() + ":" + socket.getPort());
+
+                // Construct handler to process the HTTP request message.
+                try {
+                    HttpRequestHandler request = new HttpRequestHandler(socket);
+                    // Create a new thread to process the request.
+                    Thread thread = new Thread(request);
+
+                    // Start the thread.
+                    thread.start();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
